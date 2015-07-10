134b71d4c176355fd212c80a98b9a4403f1a8975
c9katayama <c95029@gmail.com>
2015-07-10 12:27:56 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,60 +1,29 @@
 #!/bin/sh
-
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 
-eb_deploy(){
-	echo $KEY_NAME
-	
-	mkdir target
-	cd target
-	mkdir .elasticbeanstalk
-	cd .elasticbeanstalk
-	
-	cat << EOF > config.yml
-	branch-defaults:
-	  default:
-	    environment: null
-	global:
-	  application_name: $APP
-	  default_ec2_keyname: $KEY_NAME
-	  default_platform: 64bit Amazon Linux 2015.03 v1.4.1 running Docker 1.6.0
-	  default_region: ap-northeast-1
-	  profile: null
-	  sc: null
-	EOF
-	cd ..
-	
-	eb use ci-env-blue
-	eb deploy
-}
 case $TRAVIS_BRANCH in
     master)
         echo "buid and test start."
-		AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESSKEY
-		AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRETKEY
+		AWS_ACCESS_KEY_ID=$STAGING_AWS_ACCESSKEY
+		AWS_SECRET_ACCESS_KEY=$STAGING_AWS_SECRETKEY
         ./gradlew build
         exit 0
         ;;
     staging-deploy)
         echo "deploy to staging"
-		APP="CI"
-		KEY_NAME=$PROD_KEYNAME
 		AWS_ACCESS_KEY_ID=$STAGING_AWS_ACCESSKEY
 		AWS_SECRET_ACCESS_KEY=$STAGING_AWS_SECRETKEY
-		eb_deploy
+         ./eb_deploy.sh -a CI -e ci-env-green -k $STAGING_KEYNAME	
         exit 0
         ;;
     prod-deploy)
         echo "deploy to PROD."
-		APP="CI"
-		KEY_NAME=$PROD_KEYNAME
 		AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESSKEY
 		AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRETKEY
-        eb_deploy
+         ./eb_deploy.sh -a CI -e ci-env-green -k $PROD_KEYNAME	
         exit 0
         ;;
     *)
         echo "canceled."
         exit 1
-        ;;
 esac
---
+++ eb_deploy.sh
@@ -0,0 +1,65 @@
+while getopts a:e:r:k:p:s: OPT
+do
+  case $OPT in
+    "a" ) EB_APP="$OPTARG";;
+    "e" ) EB_ENV="$OPTARG";;
+    "k" ) KEY_NAME="$OPTARG";;
+    "r" ) REGION="$OPTARG";;
+    "p" ) PLATFORM="$OPTARG";;
+    "s" ) AWS_PROFILE"$OPTARG";;
+     * ) echo "required:"
+         echo " [-a EB_APP] [-e EB_ENV] [-k KEY_NAME]"
+         echo "optional:"
+         echo " [-r region(default 'ap-north-east-1')]"
+         echo " [-p eb_platform(default '64bit Amazon Linux 2015.03 v1.4.1 running Docker 1.6.0')]"
+         echo " [-s aws credential profile(default 'default')]"
+         exit 1 ;;
+  esac
+done
+
+if [ -z $EB_APP ]; then
+  echo '-a EB_APP is not defined'
+  exit 1
+fi
+if [ -z $EB_ENV ]; then
+  echo '-e EB_ENV is not defined'
+  exit 1
+fi
+if [ -z $KEY_NAME ]; then
+  echo '-k KEY_NAME is not defined'
+  exit 1
+fi
+if [ -z $REGION ]; then
+	REGION="ap-northeast-1"
+fi
+if [ -z $PLATFORM ]; then
+   PLATFORM="64bit Amazon Linux 2015.03 v1.4.1 running Docker 1.6.0"
+fi
+if [ -z $AWS_PROFILE ]; then
+   AWS_PROFILE="default"
+fi
+
+EB_DIR="`pwd`/.elasticbeanstalk"
+mkdir $EB_DIR 2>/dev/null
+
+cat << EOF > $EB_DIR/config.yml
+branch-defaults:
+  default:
+    environment: $EB_ENV
+  master:
+    environment: $EB_ENV
+global:
+  application_name: $EB_APP
+  default_ec2_keyname: $KEY_NAME
+  default_platform: $PLATFORM
+  default_region: $REGION
+  profile: $AWS_PROFILE
+  sc: null
+EOF
+DATE=`date '+%Y%m%d%H%M%S'`
+COMMIT=`git log -n 1 --format=%H`
+LABEL="$DATE-$COMMIT"
+
+eb init
+eb use $EB_ENV
+eb deploy --label $LABEL
