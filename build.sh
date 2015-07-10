74a7d3780e5c9946c310b1e0714386f3c6e3c53a
c9katayama <c95029@gmail.com>
2015-07-10 10:08:33 +0900

    update

--- build.sh
+++ build.sh
@@ -1,33 +1,60 @@
 #!/bin/sh
 
-echo $PROD_AWS_ACCESSKEY
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 
-APP="CI"
-KEY_NAME=$PROD_KEYNAME
-AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESSKEY
-AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRETKEY
-
-echo $KEY_NAME
-
-mkdir target
-cd target
-mkdir .elasticbeanstalk
-cd .elasticbeanstalk
-
-cat << EOF > config.yml
-branch-defaults:
-  default:
-    environment: null
-global:
-  application_name: $APP
-  default_ec2_keyname: $KEY_NAME
-  default_platform: 64bit Amazon Linux 2015.03 v1.4.1 running Docker 1.6.0
-  default_region: ap-northeast-1
-  profile: null
-  sc: null
-EOF
-cd ..
-
-eb use ci-env-blue
-eb deploy
+eb_deploy(){
+	echo $KEY_NAME
+	
+	mkdir target
+	cd target
+	mkdir .elasticbeanstalk
+	cd .elasticbeanstalk
+	
+	cat << EOF > config.yml
+	branch-defaults:
+	  default:
+	    environment: null
+	global:
+	  application_name: $APP
+	  default_ec2_keyname: $KEY_NAME
+	  default_platform: 64bit Amazon Linux 2015.03 v1.4.1 running Docker 1.6.0
+	  default_region: ap-northeast-1
+	  profile: null
+	  sc: null
+	EOF
+	cd ..
+	
+	eb use ci-env-blue
+	eb deploy
+}
+case $TRAVIS_BRANCH in
+    master)
+        echo "buid and test start."
+		AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESSKEY
+		AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRETKEY
+        ./gradlew build
+        exit 0
+        ;;
+    staging-deploy)
+        echo "deploy to staging"
+		APP="CI"
+		KEY_NAME=$PROD_KEYNAME
+		AWS_ACCESS_KEY_ID=$STAGING_AWS_ACCESSKEY
+		AWS_SECRET_ACCESS_KEY=$STAGING_AWS_SECRETKEY
+		eb_deploy
+        exit 0
+        ;;
+    prod-deploy)
+        echo "deploy to PROD."
+		APP="CI"
+		KEY_NAME=$PROD_KEYNAME
+		AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESSKEY
+		AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRETKEY
+        eb_deploy
+        exit 0
+        ;;
+    *)
+        echo "canceled."
+        exit 1
+        ;;
+esac
